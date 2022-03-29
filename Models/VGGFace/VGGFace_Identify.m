150467c876fb536e0da94df2d9e1adfb5612dd56
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2022-03-29 01:29:34 +0100

    update

--- Models/VGGFace/VGGFace_Identify.m
+++ Models/VGGFace/VGGFace_Identify.m
@@ -1,28 +1,6 @@
 function outputID = VGGFace_Identify(trainPath, testPath, config)
 % identify test face against train database, identification is done by
-% running face verification multiple times.
-%
-%   STADARD ARGUMENTS
-%       
-%   trainPath       - can be path to image database or embedding database
-%                   
-%   testPath        - path to a folder of testing images 
-%
-%   batchSize       - number of images to be executed at once
-%                     this will be shared across multiple different
-%                     processes
-% 
-%   NAME & VALUE PAIR ARGUMENTS
-%
-%   varargin
-%       
-%       'similarity' - distance metric to measure similarity between
-%                             feature embeddings of images
-%                             options = {'euclidean','cosine','euclidean_l2'}
-%
-%       'enablePrecompute'  - precompute embeddings of the database before
-%                             identifying faces
-%       
+% running face verification multiple times.       
 
     arguments
         trainPath (1,:) char
@@ -37,9 +15,6 @@ function outputID = VGGFace_Identify(trainPath, testPath, config)
     end
     
     % progress bar
-%     f = waitbar(0,'Seting up...','Name','Do not close this tab');
-
-    % progress bar
     f = waitbar(0,"Face recognition setting up...","Name","Face Recognition",...
         'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
     setappdata(f,'canceling',0); 
@@ -51,20 +26,6 @@ function outputID = VGGFace_Identify(trainPath, testPath, config)
     targetSize = model.Layers(1,1).InputSize;      
 
     %% LOAD DATABASE EMBEDDINGS
-
-    % progress bar
-%     waitbar(0,f,'Preparing training images...');
-
-    % load database (train) images
-%     subject_count = size(trainImgSet, 4);                        % number of people within the training database
-% 
-%     % resize loaded train images to target size
-%     trainBatch = zeros([targetSize subject_count], "single");    % init rescaled block of images
-%     for i = 1:subject_count
-%         trainBatch(:,:,:,i) = imresize(trainImgSet(:,:,:,i), targetSize(1:2));
-%     end
-%     trainBatch = normalizeBatch(trainBatch);                     % subtract intensity by VGGFace1 dataset mean
-%     X1 = dlarray(trainBatch,"SSCB");                             % convert to dlarray ready for use
 
     % if train path is not embedded dataset then compute embeddings
     if ~config.embeddedDatabase
@@ -163,52 +124,6 @@ function outputID = VGGFace_Identify(trainPath, testPath, config)
     end
     
     close(f);
-%     % precompute encoding for each subject within training database
-%     if (executionEnvironment == "auto" && canUseGPU) || executionEnvironment == "gpu"
-%         X1 = gpuArray(X1);
-%     end
-%     Y1 = embedBatch(myModel, X1);
-% 
-%     % face identification by doing face verification using emedded subject
-%     outputID = [];                              % output Label ID
-%     testSlice = zeros([targetSize 1], "single");  % a face to match against entire database
-%     Y2array = zeros(size(Y1),"single");         % non-dlarray of Y2
-% 
-%     % do face verfication for each test subject
-%     total_test_subject = length(imdsTest.Files);
-%     for i = 1:total_test_subject
-% 
-% %         waitbar(i / total_test_subject, f, sprintf('Identifying subject ( %d / %d )', i, total_test_subject));
-% 
-%         % load & resize image, assign it to a 4d array representing a batch
-%         testSlice(:,:,:,1) = imresize(single(readimage(imdsTest, i)), targetSize(1:2));
-%         testSlice = normalizeBatch(testSlice);
-%         X2Slice = dlarray(testSlice,"SSCB");   % convert to dlarray
-% 
-%         % convert to gpu array if needed
-%         if (executionEnvironment == "auto" && canUseGPU) || executionEnvironment == "gpu"
-%             X2Slice = gpuArray(X2Slice);
-%         end
-% 
-%         % embed the same subject for only one time
-%         Y2Slice = extractdata(embedBatch(myModel, X2Slice));
-% 
-%         % duplicate the embedding of this subject to match the number of people
-%         % within the train database
-%         for k = 1:subject_count
-%             Y2array(:,:,:,k) = Y2Slice(:,:,:,1);
-%         end
-%         Y2 = dlarray(Y2array,"SSCB");   % convert it back to dlarray
-% 
-%         % verify between Y1 (database) and Y2 (whole batch of a same face)
-%         result = verifyBatch(myModel, Y1, Y2, [1 1], similarity_metric);
-% 
-%         id = find([result.distance] == min([result.distance],[],"all"), 1);
-%         outputID = [outputID; trainPersonID(id,:)];
-% 
-%     end
-%
-%     close(f);
 
 end
 
